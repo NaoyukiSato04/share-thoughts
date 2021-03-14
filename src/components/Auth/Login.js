02b4d51c3244cc1ea4c53c274e10b2e8277c4812
n-n-n-n <naoyukisato20@gmail.com>
2021-03-14 20:44:34 +0900

    Fix: 登録の手間省くためにデフォルトの値設定

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -38,11 +38,11 @@ export default function Login() {
               <Form onSubmit={handleSubmit}>
                 <Form.Group id="email">
                   <Form.Label>Email</Form.Label>
-                  <Form.Control type="email" ref={emailRef} required />
+                  <Form.Control type="email" ref={emailRef} required defaultValue="aaa@aaa.com"/>
                 </Form.Group>
                 <Form.Group id="password">
                   <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" ref={passwordRef} required />
+                  <Form.Control type="password" ref={passwordRef} required defaultValue="aaaaaa"/>
                 </Form.Group>
                 <Button disabled={loading} className="w-100" type="submit">
                   ログイン
