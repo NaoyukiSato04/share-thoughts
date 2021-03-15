cb74d64fd454ed4874fd544a11afa478a7f62a12
n-n-n-n <naoyukisato20@gmail.com>
2021-03-15 14:07:36 +0900

    Fix: 細かい部分の修正

--- src/components/Timeline/Header.js
+++ src/components/Timeline/Header.js
@@ -9,15 +9,17 @@ export default function Header() {
     const history = useHistory()
 
     async function handleLogout() {
-        console.log("")
-    
-        try {
-          await logout()
-          history.push("/login")
-        } catch {
-          console.log("ログアウトに失敗しました")
-        }
+      try {
+        await logout()
+        history.push("/login")
+      } catch {
+        console.log("ログアウトに失敗しました")
       }
+    }
+
+    function handleProfile() {
+      history.push("/Profile")
+    } 
     return (
         <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
             <Navbar.Brand href="#home">share-thoughts</Navbar.Brand>
@@ -25,7 +27,7 @@ export default function Header() {
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="ml-auto">
-                    <Nav.Link href="/Profile">プロフィール</Nav.Link>
+                    <Nav.Link onClick={handleProfile}>プロフィール</Nav.Link>
                     <Nav.Link onClick={handleLogout}>ログアウト</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
